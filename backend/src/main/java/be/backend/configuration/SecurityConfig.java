9b205c53a62cf6584a6c99811a413c68fd24aca7
Kiệt <chaukiet2704@gmail.com>
2026-06-12 04:27:53 +0700

    function view history

--- backend/src/main/java/be/backend/configuration/SecurityConfig.java
+++ backend/src/main/java/be/backend/configuration/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
                                 "/api/v1/auth/login",
                                 "/api/v1/auth/refresh",
                                 "/api/v1/auth/logout",
+                                "/api/v1/movies/**",
                                 "/swagger-ui.html")
                         .permitAll()
                         .anyRequest().authenticated()
